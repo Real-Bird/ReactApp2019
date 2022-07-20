c4cb8ff7a861aaf065318ee556177bfb8520607b
Real-Bird <marbil815@gmail.com>
2022-07-20 22:57:18 +0900

    22-07-20 lifecycle

--- app2019/src/routes/About.js
+++ app2019/src/routes/About.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import "./About.css";
+import LifeCycleSample from "./LifeCycleSample";
 
-function About() {
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+class About extends Component {
+  state = {
+    color: "#000000",
+  };
 
-  return (
-    <div className="about__container">
-      <span>
-        balalalalalalal
-      </span>
-    </div>
-  )
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexFlow: "column wrap",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <button onClick={this.handleClick}>Random Color</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
+  }
 }
 
 export default About;
